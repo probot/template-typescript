dbdf23a30fd416850cfb59c94ca0d3b140497360
Bex Warner <bexmwarner@gmail.com>
2018-07-05 10:30:31 -0700

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,7 +6,5 @@ test('that we can run tests', () => {
   expect(1 + 2 + 3).toBe(6)
 })
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-// For using TypeScript in your tests, Jest recommends:
+// For more information about using TypeScript in your tests, Jest recommends:
 // https://github.com/kulshekhar/ts-jest
